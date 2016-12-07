6144dddc601ed3d17ce35d7a0293eeb389d08ce2
cocohead782 <period2@Stems-iMac-7.local>
2016-12-07 08:53:25 -0700

    Added Movement.cs

---
+++ movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+			transform.Translate(Vector3.back);
+		if (Input.GetKey("a"))
+			transform.Translate(Vector3.right);
+		if (Input.GetKey("s"))
+			transform.Translate(Vector3.forward);
+		if (Input.GetKey("d"))
+			transform.Translate(Vector3.left);
+		if (Input.GetKey("q"))
+			transform.Translate(Vector3.down);
+		if (Input.GetKey("e"))
+			transform.Translate(Vector3.up);
+	}
+}
